2adf8b9b63a6177b94c9fc1e3de2ee827a3b1403
Koichiro Yokoi <unnshoyuukou4515@me.com>
2023-11-04 02:46:30 +0900

    seed fix

--- db/seeds/development/seed_01testdata.js
+++ db/seeds/development/seed_01testdata.js
@@ -8,8 +8,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        // {username: 'user1', hash_salted_password: 'password1', salt: 'salt1', email: 'user1@example.com'},
-        // {username: 'user2', hash_salted_password: 'password2', salt: 'salt2', email: 'user2@example.com'},
+        {username: 'user1', hash_salted_password: 'password1', salt: 'salt1', email: 'user1@example.com'},
+        {username: 'user2', hash_salted_password: 'password2', salt: 'salt2', email: 'user2@example.com'},
       ]);
     });
 };
--- db/seeds/development/seed_02testhistory.js
+++ db/seeds/development/seed_02testhistory.js
@@ -8,8 +8,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('visited_restaurants').insert([
-        // {user_id: 1, restaurant_id: 'r12345', rating: 4, visited_at: new Date()},
-        // {user_id: 1, restaurant_id: 'r12346', rating: 5, visited_at: new Date()},
+        {user_id: 1, restaurant_id: 'r12345', rating: 4, visited_at: new Date()},
+        {user_id: 1, restaurant_id: 'r12346', rating: 5, visited_at: new Date()},
        
       ]);
     });
