8f8edd1e7022349fa4d1328a6e7b79d914e1edb0
Koichiro Yokoi <unnshoyuukou4515@me.com>
2023-11-08 08:22:48 +0900

    6th

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,28 @@
 // Update with your config settings.
 require('dotenv').config({ path: './.env.local' });
-const pg = require('pg');
-
-pg.defaults.ssl = process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false;
-
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
+module.exports = {  
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL, // Herokuの環境変数から接続URLを取得
+      ssl: { rejectUnauthorized: false } // HerokuのSSL設定を無効化
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/production',
+    }
+  },
   development: {
     client: 'pg', // PostgreSQLを指定
     connection: {
@@ -39,25 +53,9 @@ module.exports = {
       directory: __dirname + '/db/migrations'
     },
     seeds: {
-      directory: __dirname + '/db/seeds/developent'
+      directory: __dirname + '/db/seeds/development'
     }
   },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL+ "?sslmode=require", // Herokuの環境変数から接続URLを取得
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds/production',
-    },
-    ssl: { rejectUnauthorized: false } // HerokuのSSL設定
-  
-  }
+
 };
