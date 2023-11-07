839f708b9f23ebf1ffd505b4a90b7fd89aac3eb2
Koichiro Yokoi <unnshoyuukou4515@me.com>
2023-11-08 08:59:36 +0900

    8th

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL, // Herokuの環境変数から接続URLを取得
+      connectionString:"postgres://swqnoghdlppttp:2afd14ebe48147be2e449d2d481ca0b14f3a780863553ecf3b544186cdaf213a@ec2-3-216-4-251.compute-1.amazonaws.com:5432/d1atfk2el7k7ndprocess.env.DATABASE_URL", // Herokuの環境変数から接続URLを取得
       ssl: { rejectUnauthorized: false } // HerokuのSSL設定を無効化
     },
     pool: {
