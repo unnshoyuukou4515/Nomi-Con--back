8e1fa8670194a130b30a4c07fb0ab5fae3aa2b9c
Koichiro Yokoi <unnshoyuukou4515@me.com>
2023-11-08 08:57:12 +0900

    7th

--- knexfile.js
+++ knexfile.js
@@ -23,39 +23,6 @@ module.exports = {
       directory: './db/seeds/production',
     }
   },
-  development: {
-    client: 'pg', // PostgreSQLを指定
-    connection: {
-      database: "solo1",
-      user:     process.env.DEVELOPMENT_USER||"postgres",
-      password: process.env.DEVELOPMENT_PASSWORD||"4515",
-      host:     process.env.DEVELOPMENT_HOST||"127.0.0.1",
-      port:     process.env.DEVELOPMENT_PORT||"5432"
-    },
-    migrations: {
-      directory: __dirname + '/db/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/db/seeds/development'
-    }
-  },
-
-  staging: {
-    client: 'pg', // PostgreSQLを指定
-    connection: {
-      database: process.env.STAGING_DB,
-      user:     process.env.STAGING_USER,
-      password: process.env.STAGING_PASSWORD,
-      host:     process.env.STAGING_HOST,
-      port:     process.env.STAGING_PORT
-    },
-    migrations: {
-      directory: __dirname + '/db/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/db/seeds/development'
-    }
-  },
 
 
 };
